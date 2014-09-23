a093094038a44008c3a2026abb84ec72280de95e
PhillipPeterson <majeh90210@gmail.com>
2014-09-22 20:53:41 -0600

    Stupid Github commit error
    Something about these lines showing up in the addPanel on my last commit
    so I removed them and recommited the addPanel

--- src/AddPanel.java
+++ src/AddPanel.java
@@ -19,14 +19,14 @@ public class AddPanel extends JPanel implements ActionListener{
     
     public AddPanel(){
         
-<<<<<<< HEAD
+
         JScrollPane scrollPane = new JScrollPane();
         
         
         amounts = new ArrayList<JTextField>();
         ingredients = new ArrayList<JTextField>();
         
-=======
+
 		try {
 		    for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
 		        if ("Nimbus".equals(info.getName())) {
@@ -38,7 +38,7 @@ public class AddPanel extends JPanel implements ActionListener{
 		    // If Nimbus is not available, you can set the GUI to another look and feel.
 		}
 		
->>>>>>> origin/test
+
         addButton = new JButton("Add Recipe");
         addButton.addActionListener(this);
         cancelButton = new JButton("Cancel");
