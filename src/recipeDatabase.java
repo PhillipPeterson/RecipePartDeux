4278eeedbc2f24d03d6f36047a21115039ff2dab
Jeff Arends <jeff.arends@umontana.edu>
2014-09-23 19:18:17 -0600

    Altered the database to be called externally
    Initialize the database with
    RecipeDatabase RD = new RecipeDatabase("filename.db");

--- src/recipeDatabase.java
+++ src/recipeDatabase.java
@@ -1,36 +1,42 @@
 import java.sql.*;
 
-//public static DatabaseEntry[] getRecipes()
-//public static DatabaseEntry[] getRecipesWithName(String name)
-//public static DatabaseEntry[] getRecipesInCategory(String category)
-//public static Recipe readRecipe(int recipeId)
-//public static String [] getCategories()
-//public static void deleteRecipe(int recipeId)
-//public static int insertRecipe (String _recipeName, String _description, String[] _ingredients, String[] _ingredientAmounts, String[] _categories, String _directions), returns recipeId of the new recipe
-//public static int updateRecipe(Recipe recipe), returns recipeId of the new recipe
+// create a recipeDatabase objected using the constructor
+// RecipeDatabase DB = new RecipeDatabase("filename.db");
 
-public class recipeDatabase {
-    private static String sTempDb = "recipe.db";
-    private static Connection connection;
-    private static Statement stmt;
+//public DatabaseEntry[] getRecipes()
+//public DatabaseEntry[] getRecipesWithName(String name)
+//public DatabaseEntry[] getRecipesInCategory(String category)
+//public Recipe readRecipe(int recipeId)
+//public String [] getCategories()
+//public void deleteRecipe(int recipeId)
+//public int insertRecipe (String _recipeName, String _description, String[] _ingredients, String[] _ingredientAmounts, String[] _categories, String _directions), returns recipeId of the new recipe
+//public int updateRecipe(Recipe recipe), returns recipeId of the new recipe
 
+public class RecipeDatabase {
+    private String dbFileName = "recipe.db";
+    private Connection connection;
+    private Statement stmt;
 
+    public RecipeDatabase(String _fileName) {
+        dbFileName = _fileName;
+        init();
+    }
+    
     public static void main(String[] args) {
         try {
-            init();
+            RecipeDatabase RD = new RecipeDatabase("recipe.db");
+            RD.init();
 
 
-            printRecipes();
-            stmt.executeUpdate("delete from recipes;");
-            stmt.executeUpdate("delete from recipes_categories;");
-            stmt.executeUpdate("delete from categories;");
-            insertRecipe("ramen noodles", "dehydrated salty noodles", new String[]{"dry ramen", "water"}, new String[]{"one package", "1 cup"}, new String[]{"snacks"}, "boil water, place ramen in boiling water, consume");
-            insertRecipe("soup", "canned soup", new String[]{"soup", "water"}, new String[]{"one can", "one cup"}, new String[]{"snacks","side dishes"}, "combine ingredients, add heat");
-            printRecipes();
+            RD.printRecipes();
+            RD.stmt.executeUpdate("delete from recipes;");
+            RD.insertRecipe("ramen noodles", "dehydrated salty noodles", new String[]{"dry ramen", "water"}, new String[]{"one package", "1 cup"}, new String[]{"snacks"}, "boil water, place ramen in boiling water, consume");
+            RD.insertRecipe("soup", "canned soup", new String[]{"soup", "water"}, new String[]{"one can", "one cup"}, new String[]{"snacks","side dishes"}, "combine ingredients, add heat");
+            RD.printRecipes();
 
 
-            for(DatabaseEntry recipe:getRecipesWithName("s")) {
-                System.out.println(readRecipe(recipe.id).toString());
+            for(DatabaseEntry recipe:RD.getRecipesWithName("s")) {
+                System.out.println(RD.readRecipe(recipe.id).toString());
             }
 
         }
@@ -39,21 +45,21 @@ public class recipeDatabase {
         }
 }
 
-    public static void init() throws Exception {
-        String sDbUrl;
-        int iTimeout = 30;
-
-        // register the driver
-        String sDriverName = "org.sqlite.JDBC";
-        Class.forName(sDriverName);
-
-        // now we set up a set of fairly basic string variables to use in the body of the code proper
-        String sJdbc = "jdbc:sqlite";
-        // which will produce a legitimate Url for SqlLite JDBC :
-        // jdbc:sqlite:test.db
-        sDbUrl = sJdbc + ":" + sTempDb;
-
+    public void init() {
         try {
+            String sDbUrl;
+            int iTimeout = 30;
+
+            // register the driver
+            String sDriverName = "org.sqlite.JDBC";
+            Class.forName(sDriverName);
+
+            // now we set up a set of fairly basic string variables to use in the body of the code proper
+            String sJdbc = "jdbc:sqlite";
+            // which will produce a legitimate Url for SqlLite JDBC :
+            // jdbc:sqlite:test.db
+            sDbUrl = sJdbc + ":" + dbFileName;
+
             Class.forName(sDriverName);
             connection = DriverManager.getConnection(sDbUrl);
             stmt = connection.createStatement();
@@ -63,7 +69,7 @@ public class recipeDatabase {
         }
     }
 
-    public static void createCategory(String categoryName) throws Exception{
+    public void createCategory(String categoryName) throws Exception{
         ResultSet rs = stmt.executeQuery("select count(1) from categories where name='" + categoryName +"';");
         if(rs.getInt(1) == 0) {
             stmt.executeUpdate("insert into categories (name) values ('" + categoryName + "');");
@@ -73,7 +79,7 @@ public class recipeDatabase {
         }
     }
 
-    public static String [] getCategories() {
+    public String [] getCategories() {
         try {
             ResultSet rs = stmt.executeQuery("select count(1) from categories");
             if (rs == null) {
@@ -94,7 +100,7 @@ public class recipeDatabase {
         return null;
     }
 
-    public static void deleteRecipe(int recipeId) {
+    public void deleteRecipe(int recipeId) {
         try {
             stmt.executeUpdate("delete from recipes where id = " + recipeId + ";");
         }
@@ -103,7 +109,7 @@ public class recipeDatabase {
         }
     }
 
-    public static int insert(String table, String name) throws Exception{
+    public int insert(String table, String name) throws Exception{
         int id = 0;
         ResultSet rs = stmt.executeQuery("select count(1) from " + table + " where name='" + name +"';");
         if(rs.getInt(1) == 0) {
@@ -118,7 +124,7 @@ public class recipeDatabase {
         return id;
     }
 
-    public static String databaseEntryArrayToString(DatabaseEntry[] arr) {
+    public String databaseEntryArrayToString(DatabaseEntry[] arr) {
         String output = "";
         for(DatabaseEntry entry: arr) {
             output += entry.id + ": " + entry.name + ", ";
@@ -126,7 +132,7 @@ public class recipeDatabase {
         return output;
     }
 
-    public static int insertRecipe (String _recipeName, String _description, String[] _ingredients, String[] _ingredientAmounts, String[] _categories, String _directions) {
+    public int insertRecipe (String _recipeName, String _description, String[] _ingredients, String[] _ingredientAmounts, String[] _categories, String _directions) {
         int recipeId = 0;
         try {
 
@@ -149,7 +155,7 @@ public class recipeDatabase {
         return recipeId;
     }
 
-    public static Recipe readRecipe(int recipeId) {
+    public Recipe readRecipe(int recipeId) {
         try {
             //first read the name
             ResultSet rs = stmt.executeQuery("select * from recipes where id = '" + recipeId + "';");
@@ -198,7 +204,7 @@ public class recipeDatabase {
     }
 
     // outputs an array of DatabaseEntry objects, sorted by name
-    public static DatabaseEntry[] getRecipes() {
+    public DatabaseEntry[] getRecipes() {
         try {
             ResultSet rs = stmt.executeQuery("select count(1) from recipes;");
             DatabaseEntry[] output = new DatabaseEntry[rs.getInt(1)];
@@ -214,15 +220,20 @@ public class recipeDatabase {
         return null;
     }
 
-    public static void printRecipes() throws Exception{
-        DatabaseEntry[] cookbook = getRecipes();
-        System.out.println("Printing " + cookbook.length + " recipes");
-        for(DatabaseEntry recipe: cookbook) {
-            System.out.println(recipe.id + ": " + recipe.name + " - " + recipe.description);
+    public void printRecipes() {
+        try {
+            DatabaseEntry[] cookbook = getRecipes();
+            System.out.println("Printing " + cookbook.length + " recipes");
+            for (DatabaseEntry recipe : cookbook) {
+                System.out.println(recipe.id + ": " + recipe.name + " - " + recipe.description);
+            }
+        }
+        catch(Exception e) {
+            e.printStackTrace();
         }
     }
 
-    public static DatabaseEntry[] getRecipesInCategory(String category) throws Exception {
+    public DatabaseEntry[] getRecipesInCategory(String category) throws Exception {
         try {
             ResultSet rs = stmt.executeQuery("select count(1) from recipes_categories " +
                     "join categories on recipes_categories.categoryId = categories.id " +
@@ -245,7 +256,7 @@ public class recipeDatabase {
         return null;
     }
 
-    public static DatabaseEntry[] getRecipesWithName(String name) {
+    public DatabaseEntry[] getRecipesWithName(String name) {
         try {
             ResultSet rs = stmt.executeQuery("select count(1) from recipes where name like '%" + name + "%';");
             DatabaseEntry[] output = new DatabaseEntry[rs.getInt(1)];
@@ -262,7 +273,7 @@ public class recipeDatabase {
         return null;
     }
 
-    public static int updateRecipe(Recipe recipe) {
+    public int updateRecipe(Recipe recipe) {
         deleteRecipe(recipe.recipeId);
         String [] ingredients = new String[recipe.ingredients.length];
         for(int i = 0; i < ingredients.length; i++) {
